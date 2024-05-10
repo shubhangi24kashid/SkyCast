00515112d1f36df456596c2bc87eab7c71541d47
Zein-Zeus <zenianouphal5@gmail.com>
2024-05-10 18:09:07 +0530

    updated main.py

--- tkinter-app/main.py
+++ tkinter-app/main.py
@@ -14,10 +14,7 @@ def validate_input():
             entry.configure(bg="red")
             messagebox.showerror("Error", "Please enter all information.")
             return False
-        elif not value.replace('.', '', 1).isdigit():  # Check if value is numeric
-            entry.configure(bg="red")
-            messagebox.showerror("Error", "Please enter numeric values only.")
-            return False
+        
 
     return True
 
@@ -27,10 +24,14 @@ def calculate_result():
         return
 
     # Retrieve the input values
-    input_values = [entry.get() for entry in entry_fields]
+    input_values = []
+    for entry in entry_fields:
+        value = entry.get()
+        input_values.append(value)
 
     # Call the function from calculation.py
     result = calculation.process_inputs(input_values)
+
 
     # Update the weather label
     weather_label.configure(text="Weather: " + result)
@@ -41,22 +42,24 @@ window.title("Weather Prediction App")
 
 # Create input fields
 entry_fields = []
-input_names = ["Date (yyyy-mm-dd)", "Precipitation", "Temp Max", "Temp Min", "Wind"]
-for i, name in enumerate(input_names):
-    label = tk.Label(window, text=name + ":")
-    label.grid(row=i, column=0, padx=10, pady=5, sticky='w')
+input_names = ["Date (yyyy-mm-dd)","precipitation", "temp_max", "temp_min", "wind"]
+for i in range(5):
+    label = tk.Label(window, text=input_names[i] + ":")
+    label.grid(row=i, column=0, padx=10, pady=10)
     entry = tk.Entry(window)
-    entry.grid(row=i, column=1, padx=10, pady=5)
+    entry.grid(row=i, column=1, padx=10, pady=10)
     entry_fields.append(entry)
 
 # Create a button to calculate the result
 calculate_button = tk.Button(window, text="Predict", command=calculate_result)
 calculate_button.configure(bg="blue", fg="white")  # Set background color to blue and foreground (text) color to white
-calculate_button.grid(row=len(input_names), columnspan=2, padx=10, pady=10)
+calculate_button.grid(row=5, columnspan=2, padx=10, pady=10)
+
 
 # Create a label to display the weather
 weather_label = tk.Label(window, text="Weather: ")
-weather_label.grid(row=len(input_names) + 1, columnspan=2, padx=10, pady=5)
+weather_label.grid(row=7, columnspan=2, padx=10, pady=10)
 
 # Start the Tkinter event loop
 window.mainloop()
+
